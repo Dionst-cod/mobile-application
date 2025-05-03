74f4e8f0bab1d400815d7ee16625d766eec12c5b
Justin-1095970 <1095970@hr.nl>
2025-05-03 17:06:09 +0200

    homepage.tsx initially works will work out from here

---
+++ werkplaats4react/app/(tabs)/homepage.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomePage() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello World!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center' },
+    text: { fontSize: 20 },
+})
